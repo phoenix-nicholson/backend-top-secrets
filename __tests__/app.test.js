03676ced89541b75966ce9187aa357776ecc59c7
Phoenix Nicholson <phoenix.nicholson2008@gmail.com>
2022-03-29 15:48:23 -0700

    wrote test for sign in exsisting user

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,6 +2,8 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const UserService = require('../lib/services/UserService');
+const req = require('express/lib/request');
 
 describe('backend-top-secrets routes', () => {
   beforeEach(() => {
@@ -17,5 +19,18 @@ describe('backend-top-secrets routes', () => {
       .send({ email: 'miklo', password: 'imkindacute' });
 
     expect(res.body).toEqual({ id: expect.any(String), email: 'miklo' });
+  });
+
+  it('should be able to sign in existing user', async () => {
+    const user = await UserService.create({
+      email: 'miklo',
+      password: 'imkindacute',
+    });
+    const res = await request(app)
+      .post('/api/v1/users/')
+      .send({ email: 'miklo', password: 'imkindacute' });
+    expect(res.body).toEqual({
+      user,
+    });
   });
 });
