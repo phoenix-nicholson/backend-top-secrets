fad54f7edd2cfedc85ee8eae15d42706d5fbef32
Phoenix Nicholson <phoenix.nicholson2008@gmail.com>
2022-03-30 15:07:53 -0700

    inserting title and description into secrets table

--- sql/setup.sql
+++ sql/setup.sql
@@ -13,5 +13,11 @@ CREATE TABLE secrets (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     title TEXT NOT NULL,
     description TEXT NOT NULL,
-    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+
+INSERT INTO 
+secrets (title, description)
+VALUES
+('secret1', 'i am thing 1')
+('secret2', 'i am thing 2')
